d32292dd90b52db45cf56a4b84434d9f866ce7ed
Matas Kulkovas <mku997@gmail.com>
2018-08-01 00:13:21 +0300

    Added deletion synchronization with locks

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"runtime"
+	"sync"
 	"time"
 )
 
 type TopicBroker struct {
-	Topics map[string]Topic
+	Topics     map[string]Topic
+	DeleteLock *sync.Mutex
 }
 
 type Topic struct {
 	Listeners map[chan []byte]bool
 	Messages  chan []byte
+	Quit      chan bool
+	lock      *sync.Mutex
 }
-
 
 func EventString(message []byte, event string) string {
 	var buffer bytes.Buffer
@@ -44,21 +48,27 @@ func (broker *TopicBroker) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(req.Body)
 		s := buf.Bytes()
+		broker.DeleteLock.Lock()
 		if topic, ok := broker.Topics[topicName]; ok {
 			topic.Messages <- s
 		}
+		broker.DeleteLock.Unlock()
 		rw.WriteHeader(204)
 		return
 	}
 
 	messageChannel := make(chan []byte)
 	topicName := getPath(req)
+	broker.DeleteLock.Lock()
 	if topic, ok := broker.Topics[topicName]; ok {
+		broker.Topics[topicName].lock.Lock()
 		topic.Listeners[messageChannel] = true
+		broker.Topics[topicName].lock.Unlock()
 	} else {
 		CreateTopic(broker, topicName)
 		broker.Topics[topicName].Listeners[messageChannel] = true
 	}
+	broker.DeleteLock.Unlock()
 	connectionTime := time.Now()
 
 	for {
@@ -67,8 +77,17 @@ func (broker *TopicBroker) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 			fmt.Fprintf(rw, "%s\n", EventString(m, "msg"))
 		default:
 			if TimeoutCheck(connectionTime) {
+				broker.DeleteLock.Lock()
+				broker.Topics[topicName].lock.Lock()
 				delete(broker.Topics[topicName].Listeners, messageChannel)
+				if (len(broker.Topics[topicName].Listeners)) == 0 {
+					fmt.Println("Deleting")
+					broker.Topics[topicName].Quit <- true
+					delete(broker.Topics, topicName)
+				}
 				fmt.Fprintf(rw, "%s\n", EventString([]byte("Timeout"), "timeout"))
+				broker.DeleteLock.Unlock()
+				broker.Topics[topicName].lock.Unlock()
 				return
 			}
 		}
@@ -81,7 +100,7 @@ func (broker *TopicBroker) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 func TimeoutCheck(connectionTime time.Time) bool {
 	currentTime := time.Now()
 	delta := currentTime.Sub(connectionTime)
-	if delta.Seconds() >= 10 {
+	if delta.Seconds() >= 5 {
 		return true
 	}
 	return false
@@ -95,6 +114,8 @@ func CreateTopic(broker *TopicBroker, topic string) {
 	newTopic := &Topic{
 		Listeners: make(map[chan []byte]bool),
 		Messages:  make(chan []byte),
+		Quit:      make(chan bool, 1),
+		lock:      new(sync.Mutex),
 	}
 
 	broker.Topics[topic] = (*newTopic)
@@ -103,18 +124,25 @@ func CreateTopic(broker *TopicBroker, topic string) {
 
 func (topic *Topic) listen() {
 	for {
+		topic.lock.Lock()
 		select {
+		case <-topic.Quit:
+			defer topic.lock.Unlock()
+			return
 		case message := <-topic.Messages:
 			for listener := range topic.Listeners {
 				listener <- message
 			}
+		default:
 		}
+		topic.lock.Unlock()
 	}
 }
 
 func NewBroker() (broker *TopicBroker) {
 	broker = &TopicBroker{
-		Topics: make(map[string]Topic),
+		Topics:     make(map[string]Topic),
+		DeleteLock: new(sync.Mutex),
 	}
 
 	return
@@ -122,6 +150,21 @@ func NewBroker() (broker *TopicBroker) {
 
 func main() {
 	broker := NewBroker()
-
+	go func() {
+		for {
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+			fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+			fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+			fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+			fmt.Printf("\tNumGC = %v\n", m.NumGC)
+			time.Sleep(time.Second)
+		}
+	}()
 	http.ListenAndServe(":2666", broker)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
